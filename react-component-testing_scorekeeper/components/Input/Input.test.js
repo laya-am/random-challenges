0fc919bdb3b89a8f547e9d15f99a8d374480c953
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-08-10 15:23:19 +0200

    add testing to player component but needs fixing

--- react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -3,21 +3,21 @@ import userEvent from "@testing-library/user-event";
 import Input from ".";
 
 test("renders a label and an input with the correct attributes", () => {
-    render(<Input labelText="enter email" name="email" placeholder="enter email" value="value" required="required" />)
+    render(<Input labelText="enter email" name="email" placeholder="enter email" required="required" />)
     const label= screen.getByText(/enter email/i)
     const input= screen.getByRole("textbox", {name: /enter email/i})
-
+ 
     expect(input).toHaveAttribute("id", "email")
     expect(input).toHaveAttribute("name", "email")
     expect(input).toHaveAttribute("placeholder", "enter email")
     expect(input).toHaveAttribute("type", "text")
-    expect(input).toHaveAttribute("value", "value")
+    // expect(input).toHaveAttribute("value", "value")
     // expect(label).toHaveAttribute("htmlFor", "email")
 });
 
 test("calls callback on every user input", async () => {
     const handleChange = jest.fn();
-    render(<Input labelText="enter email" onChange={handleChange} name="email" placeholder="enter email" value="value" />)
+    render(<Input labelText="enter email" onChange={handleChange} name="email" placeholder="enter email" />)
     const input= screen.getByRole("textbox", {name: /enter email/i})
     const user = userEvent.setup();
     
