0773b7b41ad27b36fd632774024512e8063b6936
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-09-22 11:27:15 +0200

    make all routes dynamic

--- lotr-app/pages/volumes/index.js
+++ lotr-app/pages/volumes/index.js
@@ -1,19 +1,18 @@
 import Link from "next/link"
+import { volumes } from "@/lib/data"
 
 export default function Volumes() {
+
   return (
     <div>
         <h2>Volumes</h2>
         <ul>
-            <li>
-                <Link href="/volumes/the-fellowship-of-the-ring">theFellowshipOfTheRing</Link>
-            </li>
-            <li>
-                <Link href="/volumes/the-two-towers">theTwoTowers</Link>
-            </li>
-            <li>
-                <Link href="/volumes/the-return-of-the-king">theReturnOfTheKing</Link>
-            </li>
+            {volumes.map(volume => {
+            return <li key={volume.slug}>
+                <Link href={`volumes/${volume.slug}`}>{volume.title}</Link>
+                </li>
+            }
+            )}
         </ul>
     </div>
   )
