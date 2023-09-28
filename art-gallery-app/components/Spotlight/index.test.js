6a63eddacbd4df35a19a373a9f2a76ddeb025595
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-09-28 16:32:16 +0200

    complete user story 2 and add tests

---
+++ art-gallery-app/components/Spotlight/index.test.js
@@ -0,0 +1,15 @@
+import Spotlight from ".";
+import { screen, render } from "@testing-library/react";
+
+test("renders the image, title, and artist", ()=>{
+    render(<Spotlight image="https://example.com" title="Water Lilies" artist="Edward Monet" />);
+
+    const image= screen.getByAltText(/Water Lilies/i);
+    expect(image).toBeInTheDocument();
+    //  I don't know how to check the src
+    // expect(image).toHaveAttribute("src", "https://example.com")
+
+    const artist= screen.getByRole("heading", {name: /Edward Monet/i});
+    expect(artist).toBeInTheDocument();
+
+})
