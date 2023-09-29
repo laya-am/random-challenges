b15222e19bca7e483ca7d1d2099ef269b4dc729f
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-09-29 17:04:13 +0200

    add favorites page

---
+++ art-gallery-app/pages/favorites/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ArtPieces from '@/components/ArtPieces'
+
+export default function FavoritesPage({artPiecesInfo, setArtPiecesInfo, pieces}) {
+    const favPieces = pieces.filter(piece => {
+        return artPiecesInfo.find(infoPiece => piece.slug === infoPiece.slug && !!infoPiece.isFavorite)
+    })
+  return (
+    <ArtPieces pieces={favPieces} artPiecesInfo={artPiecesInfo} setArtPiecesInfo={setArtPiecesInfo} />
+  )
+}
