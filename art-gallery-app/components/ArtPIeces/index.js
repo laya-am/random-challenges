49b2958c05b7b7c48baea6b9b80e93de01d1755a
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-09-29 10:51:29 +0200

    add details page

--- art-gallery-app/components/ArtPIeces/index.js
+++ art-gallery-app/components/ArtPIeces/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ArtPiecesPreview from '../ArtPiecesPreview'
+import Link from 'next/link'
 
 export default function ArtPieces({pieces}) {
   return (
     <ul>
         {pieces.map(piece =>{
-            return <li key={piece.slug}><ArtPiecesPreview image={piece.imageSource} artist={piece.artist} title={piece.name} /></li>
+            return (<Link href={`/art-pieces/${piece.slug}`} key={piece.slug}>
+              <li>
+                <ArtPiecesPreview image={piece.imageSource} artist={piece.artist} title={piece.name} />
+              </li>
+            </Link>)
         })}
     </ul>
   )
